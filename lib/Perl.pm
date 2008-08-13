0c39cd6c88376985b42c8b4bdc5f7cacb20b9f4c
brian d foy <brian@stonehenge.com>
2008-08-13 11:03:34 +0200

    * Initial output of the module_cooker script

---
+++ lib/Perl.pm
@@ -0,0 +1,55 @@
+# $Id$
+package SourceCode::LineCounter::Perl;
+use strict;
+
+use warnings;
+no warnings;
+
+use subs qw();
+use vars qw($VERSION);
+
+$VERSION = '0.10_01';
+
+=head1 NAME
+
+SourceCode::LineCounter::Perl - This is the description
+
+=head1 SYNOPSIS
+
+	use SourceCode::LineCounter::Perl;
+
+=head1 DESCRIPTION
+
+=cut
+
+
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is part of a SourceForge project which always has the
+latest sources in CVS, as well as all of the previous releases.
+
+	http://sourceforge.net/projects/brian-d-foy/
+
+If, for some reason, I disappear from the world, one of the other
+members of the project can shepherd this module appropriately.
+
+=head1 AUTHOR
+
+brian d foy, C<< <bdfoy@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2008, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
